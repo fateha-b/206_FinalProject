8d35149149c6737c3d05bff7ecfc006869fa26d7
nritu5 <najmul13.ritu@gmail.com>
2025-04-21 11:06:00 -0400

    query for weather

--- querydata.py
+++ querydata.py
@@ -28,4 +28,16 @@ print("Top 5 Reddit Posts:")
 for row in results:
     print(f"Title: {row[0]}\nSubreddit: {row[1]}\nUpvotes: {row[2]}\nKeyword: {row[3]}\nDate: {row[4]}\n")
 
+cur.execute('''
+SELECT date, temp_max,temp_min, precipitation, humidity
+FROM Weather2025
+ORDER BY date DESC
+LIMIT 5
+''')
+weather_results = cur.fetchall()
+
+print("Recent 5 Days of Weather Data:")
+for row in weather_results:
+    print(f"Date: {row[0]}, Temp (°F): {row[1]}, Humidity: {row[2]}%, Weather: {row[3]}, Precipitation: {row[4]}mm")
+
 conn.close()
